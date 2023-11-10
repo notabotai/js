49491d3a5e6449db9a9fdb91e9e6aaec038de43e
Anup Bishnoi <pixelsallover@gmail.com>
2023-11-10 15:18:10 -0800

    add server entrypoint

---
+++ server.ts
@@ -0,0 +1,38 @@
+import {
+  Application,
+  Router,
+  ServerSentEventTarget,
+} from "https://deno.land/x/oak@v12.6.0/mod.ts";
+import staticFiles from "https://deno.land/x/static_files@1.1.6/mod.ts";
+import { debounce } from "https://deno.land/std@0.197.0/async/debounce.ts";
+
+const app = new Application();
+const router = new Router();
+
+// Static files from public/
+app.use(staticFiles("public", { cacheControl: false }));
+
+// Watch public folder and reload browser on changes
+async function watchPublicFolder() {
+  const targets: ServerSentEventTarget[] = [];
+  router.get("/last-change", (ctx) => {
+    targets.push(ctx.sendEvents());
+  });
+  const send = (event: Deno.FsEvent, lastChangeTime: Date) => {
+    console.log(
+      `[${lastChangeTime.toLocaleTimeString()}] ${event.kind} ${event.paths[0]}`
+    );
+    targets.forEach((t) => t.dispatchMessage({ data: lastChangeTime }));
+  };
+  const debouncedSend = debounce(send, 20);
+  for await (const event of Deno.watchFs("public")) {
+    // Example event: { kind: "create", paths: [ "/home/alice/deno/foo.txt" ] }
+    debouncedSend(event, new Date());
+  }
+}
+watchPublicFolder();
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen({ port: 8000 });
