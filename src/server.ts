0039ed4379f7e7b1ecfda512baf756c1c00d7efa
Anup Bishnoi <pixelsallover@gmail.com>
2024-01-13 22:59:09 -0800

    refactored server to support github pass-through

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,26 @@
-export const routes = new Map<
-  string,
-  (requestEvent: Deno.RequestEvent) => Promise<void>
->();
+import { BufReader, readLines } from "https://deno.land/std@0.212.0/io/mod.ts";
+
+type HttpHeaders = Record<string, string>;
+type HttpContent = string | ReadableStream<Uint8Array> ;
+type RouteHandler = (requestEvent: Deno.RequestEvent) => Promise<HttpResponse> | HttpResponse;
+
+class HttpResponse {
+  content: HttpContent = "";
+  headers: HttpHeaders = {};
+  status = 200;
+  constructor(content: HttpContent, headers: HttpHeaders = {}, status = 200) {
+    this.content = content;
+    this.headers = headers;
+    this.status = status;
+  }
+};
+
+export const routes = new Map<string, RouteHandler>();
 export const openConnections: ReadableStreamDefaultController[] = [];
 export const isProd = Deno.env.get("DENO_DEPLOYMENT_ID") !== undefined;
+
+const response404: HttpResponse = new HttpResponse("404 Not Found", { "Content-Type": "text/plain" }, 404);
+const response500: HttpResponse = new HttpResponse("500 Internal Server Error", { "Content-Type": "text/plain" }, 500);
 
 export function watchDevDirs({
   dirs = ["src", "public"],
@@ -20,9 +37,9 @@ export function watchDevDirs({
     }
   }
 
-  routes.set("/last-change", async (requestEvent) => {
+  routes.set("/last-change", (_requestEvent) => {
     let connection: ReadableStreamDefaultController | undefined;
-    const body = new ReadableStream({
+    const content = new ReadableStream({
       start(controller) {
         connection = controller;
         openConnections.push(connection);
@@ -35,17 +52,7 @@ export function watchDevDirs({
         }
       },
     });
-    try {
-      await requestEvent.respondWith(
-        new Response(body, {
-          headers: {
-            "Content-Type": "text/event-stream",
-          },
-        })
-      );
-    } catch (_err) {
-      // Ignore local errors
-    }
+    return new HttpResponse(content, { "Content-Type": "text/event-stream" });
   });
 
   async function watchDir(dir: string) {
@@ -93,80 +100,109 @@ export function watchDevDirs({
 export async function serveHttpRequests({
   port = Deno.env.get("PORT") ?? 8000,
   publicDir = "public",
+  githubRepo = "notabotai/js",
 } = {}) {
   for await (const conn of Deno.listen({ port: +port })) {
-    try {
-      handleHttp(conn).catch(console.error);
-    } catch (err) {
-      console.error("handleHttp failure", err);
+    const httpConn = Deno.serveHttp(conn);
+    for await (const requestEvent of httpConn) {
+      try {
+        await sendResponse(requestEvent, await handleRequest(requestEvent))
+      } catch (err) {
+        console.error("request handling failure", err);
+        await sendResponse(requestEvent, response500);
+      }
     }
   }
 
-  async function handleHttp(conn: Deno.Conn) {
-    const httpConn = Deno.serveHttp(conn);
-    for await (const requestEvent of httpConn) {
-      const url = new URL(requestEvent.request.url);
-      let route = decodeURIComponent(url.pathname);
+  async function sendResponse(requestEvent: Deno.RequestEvent, response: HttpResponse) {
+    const { content, status, headers } = response;
+    await requestEvent.respondWith(new Response(content, { status, headers }));
+  }
 
-      // routes
-      if (routes.has(route)) {
-        await routes.get(route)!(requestEvent);
-        continue;
+  async function handleRequest(requestEvent: Deno.RequestEvent) {
+    const url = new URL(requestEvent.request.url);
+    let route = decodeURIComponent(url.pathname);
+
+    const defaultHeaders: HttpHeaders = {
+      "Content-Type": getMimeType(route.split(".").pop()),
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+      "Access-Control-Allow-Headers": "Content-Type",
+    };
+    console.log("request", route);
+
+    // defined routes
+    if (routes.has(route)) {
+      console.log("route", route);
+      const response = await routes.get(route)!(requestEvent);
+      response.headers = { ...defaultHeaders, ...response.headers };
+      return response;
+    }
+
+    // stream content from github (by commit)
+    if (route.startsWith("/gh/")) {
+      console.log("github", route);
+      const ghUrl = route.replace("/gh/", `https://raw.githubusercontent.com/${githubRepo}/`);
+      const content = (await fetch(ghUrl)).body;
+      if (content) {
+        return new HttpResponse(content, defaultHeaders);
       }
+      return response404;
+    }
 
-      // index.html
-      if (route === "/") {
-        route = "/index.html";
-      }
-
-      // html files get templated
-      if (route.endsWith(".html")) {
-        // read file and replace __js__ with js/ import map source
-        const content = await Deno.readTextFile(publicDir + route);
-        let resourceUrl = "";
-        try {
-          resourceUrl = JSON.parse(await Deno.readTextFile("./deno.json")).imports["js/"];
-        } catch (err) {
-          console.error("Failed to read deno.json for import map", err);
-        }
-        const response = content.replaceAll("__js__", resourceUrl);
-        await requestEvent.respondWith(
-          new Response(response, {
-            headers: {
-              "Content-Type": "text/html",
-            },
-          })
-        );
-        continue;
-      }
-
-      // static files
-      let file: Deno.FsFile;
-      let typesFile: Deno.FsFile | undefined;
+    // static files
+    if (route === "/") route = "/index.html"; // default to index.html
+    let file: Deno.FsFile | undefined;
+    let typesFile: Deno.FsFile | undefined;
+    try {
+      file = await Deno.open(publicDir + route, { read: true });
+    } catch {
+      console.log("404", route);
+      return response404;
+    }
+    if (route.endsWith(".js")) {
       try {
-        file = await Deno.open(publicDir + route, { read: true });
-        if (route.endsWith(".js")) {
-          typesFile = await Deno.open(publicDir + route.replace(/\.js$/, ".d.ts"), { read: true });
-        }
+        typesFile = await Deno.open(publicDir + route.replace(/\.js$/, ".d.ts"), { read: true });
       } catch {
-        // If the file cannot be opened, return a "404 Not Found" response
-        const notFoundResponse = new Response("404 Not Found", { status: 404 });
-        await requestEvent.respondWith(notFoundResponse);
-        continue;
+        // no types known for this JS file
       }
-      const fileExt = route.split(".").pop();
-      const headers = {
-        "Content-Type": getMimeType(fileExt),
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-        "Access-Control-Allow-Headers": "Content-Type",
-        "X-TypeScript-Types": "",
-      };
+    }
+
+    // read file, replace __js__ substrings with js/ import map from deno.json
+    if (file && route.endsWith(".html")) {
+      console.log("html", route);
+      let importDomain = "";
+      try {
+        importDomain = JSON.parse(await Deno.readTextFile("./deno.json")).imports["js/"];
+      } catch (err) {
+        console.error("Failed to read deno.json for import map", err);
+      }
+      const reader = new BufReader(file);
+      const encoder = new TextEncoder();
+      const response = new ReadableStream({
+        async start(controller) {
+          for await (const line of readLines(reader)) {
+            const modifiedLine = line.replaceAll("__js__", importDomain);
+            controller.enqueue(encoder.encode(modifiedLine + "\n"));
+          }
+          controller.close();
+          if (file) file.close();
+        }
+      });
+      return new HttpResponse(response, defaultHeaders);
+    }
+
+    // static local files
+    if (file) {
+      console.log("static", route);
+      const headers = { ...defaultHeaders };
       if (typesFile) {
         headers["X-TypeScript-Types"] = route.replace(/\.js$/, ".d.ts");
       }
-      await requestEvent.respondWith(new Response(file.readable, { headers }));
+      return new HttpResponse(file.readable, headers);
     }
+
+    return response404;
   }
 }
 
