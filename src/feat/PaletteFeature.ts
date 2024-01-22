af5d0c255eb119f45a9b159b91a623793a5ca639
Anup Bishnoi <pixelsallover@gmail.com>
2024-01-21 23:58:45 -0800

    new colors in palette

--- src/feat/PaletteFeature.ts
+++ src/feat/PaletteFeature.ts
@@ -24,6 +24,30 @@ type Palette = {
   gray: string;
   grayDark: string;
   grayLight: string;
+  red: string;
+  redTransparent: string;
+  redDark: string;
+  redLight: string;
+  green: string;
+  greenTransparent: string;
+  greenDark: string;
+  greenLight: string;
+  blue: string;
+  blueTransparent: string;
+  blueDark: string;
+  blueLight: string;
+  yellow: string;
+  yellowTransparent: string;
+  yellowDark: string;
+  yellowLight: string;
+  purple: string;
+  purpleTransparent: string;
+  purpleDark: string;
+  purpleLight: string;
+  orange: string;
+  orangeTransparent: string;
+  orangeDark: string;
+  orangeLight: string;
   white: string;
 };
 export type PaletteColor = keyof Palette;
@@ -59,7 +83,31 @@ export class PaletteFeature extends Feature {
       black: "#272829",
       gray: "#61677A",
       grayDark: "#27374D",
-      grayLight: "#D8D9DA",
+      grayLight: "#CFCFC4",
+      red: "#FF6961", // https://www.colorhexa.com/ff6961
+      redTransparent: "#FF696177",
+      redDark: "#B00900",
+      redLight: "#FFB4B0",
+      green: "#61FF69",
+      greenTransparent: "#61FF6977",
+      greenDark: "#00B009",
+      greenLight: "#B0FFB4",
+      blue: "#6161FF",
+      blueTransparent: "#6161FF77",
+      blueDark: "#0000B0",
+      blueLight: "#B0B0FF",
+      yellow: "#FFB861",
+      yellowTransparent: "#FFB86177",
+      yellowDark: "#B06100",
+      yellowLight: "#FFDBB0",
+      purple: "#FF61F7",
+      purpleTransparent: "#FF61F777",
+      purpleDark: "#B000A7",
+      purpleLight: "#FFB0FB",
+      orange: "#FFB347",
+      orangeTransparent: "#FFB34777",
+      orangeDark: "#A36000",
+      orangeLight: "#FFD9A3",
       white: "#F1F6F9",
     },
   };
